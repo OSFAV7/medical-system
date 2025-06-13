1dd821837b4a4a13bf2b384b7087f20cd896c2d3
Jorge Fred Alvarez Salaya <mk.fred2@gmail.com>
2025-06-13 15:53:08 -0600

    ese tiene compoente home.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import PacienteDashboard  from './paginas/PacienteDashboard'
 import DoctorDashboard    from './paginas/DoctorDashboard'
 import FarmaciaDashboard  from './paginas/FarmaciaDashboard'
+import Home from './components/home'
 
 import { Routes, Route } from 'react-router-dom'
 
@@ -13,12 +14,12 @@ export default function App() {
 
       <main className="flex-grow flex items-center justify-center">
         <Routes>
+          
           <Route
             path="/"
             element={
-              <h1 className="text-4xl font-bold text-blue-600">
-                ¡Bienvenido al Medical!
-              </h1>
+              
+            <Home />
             }
           />
           {/* Rutas para cada rol */}
