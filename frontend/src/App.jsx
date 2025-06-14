62fc9bffe5c57b674b5e9faf15563a0825f386a7
OSFAV7 <72432488+OSFAV7@users.noreply.github.com>
2025-06-13 19:08:17 -0600

    pagina funcionando

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,21 @@
 // src/App.jsx
-import Navbar from './components/Navbar'
 import PacienteDashboard  from './paginas/PacienteDashboard'
 import DoctorDashboard    from './paginas/DoctorDashboard'
 import FarmaciaDashboard  from './paginas/FarmaciaDashboard'
 import Home from './components/home'
-
+import { DoctorDashboard } from "./paginas/DoctorDashboard";
 import { Routes, Route } from 'react-router-dom'
 
 export default function App() {
   return (
     <div className="min-h-screen flex flex-col bg-gray-100">
-      <Navbar />      {/* pónla aquí arriba */}
-
       <main className="flex-grow flex items-center justify-center">
         <Routes>
           
           <Route
             path="/"
             element={
-              
-            <Home />
+            <Home /> 
             }
           />
           {/* Rutas para cada rol */}
