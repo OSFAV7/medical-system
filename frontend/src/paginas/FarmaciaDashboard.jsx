b898bc3565fe48adf51fa59bc7854443471ebb81
OSFAV7 <72432488+OSFAV7@users.noreply.github.com>
2025-06-15 15:58:43 -0600

    faramcia

--- frontend/src/paginas/FarmaciaDashboard.jsx
+++ frontend/src/paginas/FarmaciaDashboard.jsx
@@ -1,8 +1,14 @@
+import Navbar from '../components/Navbar-sesion'
+
 export default function FarmaciaDashboard() {
   return (
-    <div className="p-4">
-      <h2 className="text-2xl font-bold">Panel del FarmaciaDashboard</h2>
-      {/* … */}
+    <div className='w-full'>
+      <Navbar />
+      <div className="min-h-screen bg-gray-100 pt-2 grid md:grid-cols-[1fr_1fr] relative">
+        
+        <h2 className="pt-40 text-2xl text-center font-bold">Panel del FarmaciaDashboard</h2>
+        {/* … */}
+      </div>
     </div>
   );
 }
